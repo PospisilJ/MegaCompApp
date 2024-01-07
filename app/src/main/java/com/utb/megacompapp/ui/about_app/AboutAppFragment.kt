1ef3274feda92709ccd1edb211929e16460fb874
PospisilJ <j1_pospisil@utb.cz>
2024-01-07 17:02:06 +0100

    String update
    update of strings

--- app/src/main/java/com/utb/megacompapp/ui/about_app/AboutAppFragment.kt
+++ app/src/main/java/com/utb/megacompapp/ui/about_app/AboutAppFragment.kt
@@ -1,4 +1,4 @@
-package com.utb.megacompapp.ui.notifications
+package com.utb.megacompapp.ui.about_app
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.utb.megacompapp.databinding.FragmentNotificationsBinding
+import com.utb.megacompapp.databinding.FragmentAboutAppBinding
 
-class NotificationsFragment : Fragment() {
+class AboutAppFragment : Fragment() {
 
-    private var _binding: FragmentNotificationsBinding? = null
+    private var _binding: FragmentAboutAppBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +22,14 @@ class NotificationsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val notificationsViewModel =
-            ViewModelProvider(this).get(NotificationsViewModel::class.java)
+        val aboutAppViewModel =
+            ViewModelProvider(this).get(AboutAppViewModel::class.java)
 
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        _binding = FragmentAboutAppBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
+        aboutAppViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
