1818e69dde4fc60617bb0d5732134d9e6da71b11
PospisilJ <j1_pospisil@utb.cz>
2024-01-07 19:21:53 +0100

    icons, background, etc

--- app/src/main/java/com/utb/megacompapp/ui/about_app/AboutAppViewModel.kt
+++ app/src/main/java/com/utb/megacompapp/ui/about_app/AboutAppViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class AboutAppViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+        value = "This is about app Fragment"
     }
     val text: LiveData<String> = _text
 }
