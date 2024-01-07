415c9c14e3f9c1e1b4ab82871a15bb2b14610d01
PospisilJ <j1_pospisil@utb.cz>
2024-01-07 20:45:20 +0100

    profile fragment edit

--- app/src/main/java/com/utb/megacompapp/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/utb/megacompapp/ui/profile/ProfileFragment.kt
@@ -27,11 +27,11 @@ class ProfileFragment : Fragment() {
 
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
         val root: View = binding.root
-
+        /*
         val textView: TextView = binding.textDashboard
         profileViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
-        }
+        }*/
         return root
     }
 
